073a36d78efe34eeaae05f21123f55c35e47cee1
pDunCanCode <chesiredarling@gmail.com>
2020-08-12 13:15:01 -0700

    add play/pause functions to videoplayer

--- videoPlayer/script.js
+++ videoPlayer/script.js
@@ -3,3 +3,44 @@ const play = document.getElementById("play");
 const stop = document.getElementById("stop");
 const progress = document.getElementById("progress");
 const timestamp = document.getElementById("timestamp");
+
+function toggleVideoStatus() {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+}
+
+function updatePlayIcon() {
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
+}
+
+function updateProgress() {
+  return true;
+}
+
+function setVideoProgress() {
+  return;
+}
+
+function stopVideo() {
+  video.currentTime = 0;
+  video.pause();
+}
+
+// Event listeners
+
+video.addEventListener("click", toggleVideoStatus);
+video.addEventListener("pause", updatePlayIcon);
+video.addEventListener("play", updatePlayIcon);
+video.addEventListener("timeUpdate", updateProgress);
+
+play.addEventListener("click", toggleVideoStatus);
+stop.addEventListener("click", stopVideo);
+
+progress.addEventListener("change", setVideoProgress);
