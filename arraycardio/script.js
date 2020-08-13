25f8b31d82da59cb891fac25239d5557c179f35c
pDunCanCode <chesiredarling@gmail.com>
2020-08-13 13:42:56 -0700

    add getRandomUser

---
+++ arraycardio/script.js
@@ -0,0 +1,32 @@
+const main = document.getElementById("main");
+const addUserBtn = document.getElementById("add-user");
+const doubleBtn = document.getElementById("double");
+const showMillionairesBtn = document.getElementById("show-millionaires");
+const sortBtn = document.getElementById("sort");
+const calculateWealthBtn = document.getElementById("calculate-wealth");
+
+let data = [];
+
+getRandomUser();
+getRandomUser();
+getRandomUser();
+
+// fetch the random user && add money value
+
+async function getRandomUser() {
+  const res = await fetch("https://randomuser.me/api");
+
+  const data = await res.json();
+  const user = data.results[0];
+
+  const newUser = {
+    name: `${user.name.first} ${user.name.last}`,
+    money: Math.floor(Math.random() * 10000000),
+  };
+
+  addData(newUser);
+}
+
+function addData(obj) {
+  data.push(obj);
+}
