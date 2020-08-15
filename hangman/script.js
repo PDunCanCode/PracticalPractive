223d86307db735676abe58db59eb340f4a3f39f5
pDunCanCode <chesiredarling@gmail.com>
2020-08-15 15:39:51 -0700

    add reset buttons functionality

--- hangman/script.js
+++ hangman/script.js
@@ -40,6 +40,78 @@ function displayWord() {
 
     playable = false;
   }
-}
 
-displayWord();
+  function updateWrongLettersEl() {
+    wrongLettersEl.innerHTML = `
+        ${wrongLetters.length > 0 ? "<p>Wrong</p>" : ""}
+        ${wrongLetters.mao((letter) => `<span>${letter}</span>`)}
+    `;
+
+    figureParts.forEach((part, index) => {
+      const errors = wrongLetters.length;
+
+      if (index < errors) {
+        part.style.display = "block";
+      } else {
+        part.style.display = "none";
+      }
+    });
+    // Check if lost
+    if (wrongLetters.length === figureParts.length) {
+      finalMessage.innerText = "Unfortunately you lost. 😕";
+      finalMessageRevealWord.innerText = `...the word was: ${selectedWord}`;
+      popup.style.display = "flex";
+
+      playable = false;
+    }
+  }
+
+  function showNotification() {
+    notification.classList.add("show");
+
+    setTimeout(() => {
+      notification.classList.remove("show");
+    }, 2000);
+  }
+
+  window.addEventListener("keydown", (e) => {
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+      const letter = e.key;
+
+      if (selectedWord.includes(letter)) {
+        if (!correctLetters.includes(letter)) {
+          correctLetters.push(letter);
+
+          displayWord();
+        } else {
+          showNotification();
+        }
+      } else {
+        if (!wrongLetters.includes(letter)) {
+          wrongLetters.push(letter);
+
+          updateWrongLettersEl();
+        }
+      }
+    }
+  });
+
+  // Restart game and play again
+  playAgainBtn.addEventListener("click", () => {
+    playable = true;
+
+    //  Empty arrays
+    correctLetters.splice(0);
+    wrongLetters.splice(0);
+
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+
+    displayWord();
+
+    updateWrongLettersEl();
+
+    popup.style.display = "none";
+  });
+
+  displayWord();
+}
