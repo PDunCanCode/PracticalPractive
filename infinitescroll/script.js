ffd9698f4fbf41c2b00a30e3186948b93bfef8d5
pDunCanCode <chesiredarling@gmail.com>
2020-08-18 11:49:22 -0700

    add update to showLoading/ setTimeout and update CSS

--- infinitescroll/script.js
+++ infinitescroll/script.js
@@ -32,6 +32,26 @@ async function showPosts() {
     postsContainer.appendChild(postEl);
   });
 }
+// show oader && fetch more posts
+function showLoading() {
+  loading.classList.add("show");
+  setTimeout(() => {
+    loading.classList.remove("show");
+
+    setTimeout(() => {
+      page++;
+      showPosts();
+    }, 300);
+  }, 1000);
+}
 
 // Show initial posts
 showPosts();
+
+window.addEventListener("scroll", () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.document.Element;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
