c4899e39d0050c569b35c960005650497b493ef8
pDunCanCode <chesiredarling@gmail.com>
2020-08-21 09:51:50 -0700

    add key functions

--- breakout/script.js
+++ breakout/script.js
@@ -31,24 +31,24 @@ const paddle = {
 
 // Create Brick properties
 const brickInfo = {
-    w: 70,
-    h: 20,
-    padding: 10,
-    offsetX: 45,
-    offSetY: 60,
-    visible: true,
-}
+  w: 70,
+  h: 20,
+  padding: 10,
+  offsetX: 45,
+  offSetY: 60,
+  visible: true,
+};
 
 //create bricks
 
 const bricks = [];
-for(let i =0; i < brickRowCount, i++) {
-    bricks[i] =[];
-    for (let j= 0; j< brickColoumnCount; j++){
-        const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
-        const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
-        bricks[i][j] = { x, y, ...brickInfo };
-    }
+for (let i = 0; i < brickRowCount; i++) {
+  bricks[i] = [];
+  for (let j = 0; j < brickColoumnCount; j++) {
+    const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+    const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+    bricks[i][j] = { x, y, ...brickInfo };
+  }
 }
 
 //Draw ball on canvas
@@ -69,8 +69,23 @@ function drawPaddle() {
   ctx.closePath();
 }
 
+//Move the paddle
+function movePAdle() {
+  paddle.x += paddle.dx;
+
+  //walldetection
+
+  if (paddle.x + paddle.w > canvas.width) {
+    paddle.x = canvas.width = paddle.w;
+  }
+  if (paddle.x < 0) {
+    paddle.x = 0;
+  }
+}
+
 // Draw all the things
 function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawBall();
   drawPaddle();
   drawScore();
@@ -83,23 +98,49 @@ function drawScore() {
 }
 //draw bricks on Canvas
 function drawBricks() {
-    bricks.forEach(column => {
-      column.forEach(brick => {
-        ctx.beginPath();
-        ctx.rect(brick.x, brick.y, brick.w, brick.h);
-        ctx.fillStyle = brick.visible ? '#0095dd' : 'transparent';
-        ctx.fill();
-        ctx.closePath();
-      });
+  bricks.forEach((column) => {
+    column.forEach((brick) => {
+      ctx.beginPath();
+      ctx.rect(brick.x, brick.y, brick.w, brick.h);
+      ctx.fillStyle = brick.visible ? "#0095dd" : "transparent";
+      ctx.fill();
+      ctx.closePath();
     });
+  });
 }
 
+function update() {
+  movePaddle();
 
+  draw();
+}
 
+update();
 
-draw();
+// Keydown event
+function keyDown(e) {
+  if (e.key === "Right" || e.key === "ArrowRight") {
+    paddle.dx = paddle.speed;
+  } else if (e.key === "Left" || e.key === "ArrowLeft") {
+    paddle.dx = -paddle.speed;
+  }
+}
 
+// Keyup event
+function keyUp(e) {
+  if (
+    e.key === "Right" ||
+    e.key === "ArrowRight" ||
+    e.key === "Left" ||
+    e.key === "ArrowLeft"
+  ) {
+    paddle.dx = 0;
+  }
+}
 
+//keyBoard Event Hndlers
+document.addEventListener("keydown", keyDown);
+document.addEventListener("keyup", keyUp);
 
 //Rules and close event handlers
 
