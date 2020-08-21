b163cefd54d6b98c4ffb783c60855b2b5ba981f3
pDunCanCode <chesiredarling@gmail.com>
2020-08-20 21:45:21 -0700

    drawBricks function

--- breakout/script.js
+++ breakout/script.js
@@ -5,6 +5,8 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 
 let score = 0;
+const brickRowCount = 9;
+const brickColoumnCount = 5;
 
 //Create ball properties
 const ball = {
@@ -26,6 +28,28 @@ const paddle = {
   speed: 8,
   dx: 0,
 };
+
+// Create Brick properties
+const brickInfo = {
+    w: 70,
+    h: 20,
+    padding: 10,
+    offsetX: 45,
+    offSetY: 60,
+    visible: true,
+}
+
+//create bricks
+
+const bricks = [];
+for(let i =0; i < brickRowCount, i++) {
+    bricks[i] =[];
+    for (let j= 0; j< brickColoumnCount; j++){
+        const x = i * (brickInfo.w + brickInfo.padding) + brickInfo.offsetX;
+        const y = j * (brickInfo.h + brickInfo.padding) + brickInfo.offsetY;
+        bricks[i][j] = { x, y, ...brickInfo };
+    }
+}
 
 //Draw ball on canvas
 function drawBall() {
@@ -50,13 +74,33 @@ function draw() {
   drawBall();
   drawPaddle();
   drawScore();
+  drawBricks();
 }
 
 function drawScore() {
   ctx.font = "20px Arial";
   ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
 }
+//draw bricks on Canvas
+function drawBricks() {
+    bricks.forEach(column => {
+      column.forEach(brick => {
+        ctx.beginPath();
+        ctx.rect(brick.x, brick.y, brick.w, brick.h);
+        ctx.fillStyle = brick.visible ? '#0095dd' : 'transparent';
+        ctx.fill();
+        ctx.closePath();
+      });
+    });
+}
+
+
+
+
 draw();
+
+
+
 //Rules and close event handlers
 
 rules.Btn.addEventListener("click", () => rules.classList.add("show"));
