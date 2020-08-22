7f674ab6613ad46ff7f15e0a4aed20290879e05c
pDunCanCode <chesiredarling@gmail.com>
2020-08-22 15:12:36 -0700

    add higher order fuctions to randomize list

--- sortlist/script.js
+++ sortlist/script.js
@@ -24,19 +24,23 @@ createList();
 //insert list items into DOM
 
 function createList() {
-  [...richestPeople].forEach((person, index) => {
-    const listItem = document.createElement("li");
+  [...richestPeople]
+    .map((a) => ({ value: a, sort: Math.random() }))
+    .sort((a, b) => a.sort - b.sort)
+    .map((a) => a.value)
+    .forEach((person, index) => {
+      const listItem = document.createElement("li");
 
-    listItem.setAttribute("data-index", index);
+      listItem.setAttribute("data-index", index);
 
-    listItem.innerHTML = `
+      listItem.innerHTML = `
             <span class="number">${index + 1}</span>
             <div class="draggable" draggable="true">
                 <p class="person-name">${person}</p>
                 <i class="fas fa-grip-lines"></i>
             </div>
         `;
-    listItems.push(listItems);
-    draggable_list.appendChild();
-  });
+      listItems.push(listItems);
+      draggable_list.appendChild();
+    });
 }
