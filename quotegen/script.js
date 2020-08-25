4e44fa56838a2a42758dc0107dbd134fc81d015a
pDunCanCode <chesiredarling@gmail.com>
2020-08-24 18:57:37 -0700

    add JS async function getQuote

--- quotegen/script.js
+++ quotegen/script.js
@@ -4,3 +4,21 @@ const authorText = document.getElementById("author");
 const twitterBtn = document.getElementById("twitter");
 const newQuoteBtn = document.getElementById("new-quote");
 const loader = document.getElementById("loader");
+
+//Get a quote from the api
+async function getQuote() {
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const apiUrl =
+    "https://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json";
+  try {
+    const response = await fetch(proxyUrl + apiUrl);
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    getQuote();
+    console.log("shit fucked up, yo", error);
+  }
+}
+
+// On Load
+getQuote();
