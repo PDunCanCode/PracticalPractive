a69f7776df8a7db7133115a73c5e9f450007d148
pDunCanCode <chesiredarling@gmail.com>
2020-08-25 15:52:10 -0700

    add displayPhotos function

--- infinteSkroll/script.js
+++ infinteSkroll/script.js
@@ -1,5 +1,37 @@
 const imageContainer = document.getElementById("image-container");
+const laoder = document.getElementById("loader");
 
+let ready = false;
+let imagesLoaded = 0;
+let totalImages = 0;
+let photosArray = [];
+
+//Helper function
+function setAttributes(element, attributes) {
+  for (const key in attributes) {
+    element.setAttribute(key, attribute[key]);
+  }
+}
+
+// Create Elements for Links
+function displayPhotos() {
+  photosArray.forEach((photo) => {
+    const item = document.createElement("a");
+
+    setAttributes(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+    const img = document.createElement("img");
+    setAttributes(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
+}
 // Unsplash API
 const count = 30;
 // Normally, don't store API Keys like this, but an exception made here because it is free, and the data is publicly available!
@@ -11,7 +43,7 @@ const apiUrl = `https://api.unsplash.com/photos/random?client_id=${apiKey}&count
 async function getPhotos() {
   try {
     const response = await fetch(apiUrl);
-    const data = await response.json();
+    const photosArray = await response.json();
   } catch (error) {}
 }
 
