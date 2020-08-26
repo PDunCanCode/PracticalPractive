a55793b5c486cff6bb18bae142370182f7fc8d4d
pDunCanCode <chesiredarling@gmail.com>
2020-08-26 14:58:32 -0700

    add initial JS functions

--- darkmode/script.js
+++ darkmode/script.js
@@ -1,1 +1,11 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
+function switchTheme(event) {
+  if (event.target.checked) {
+    document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
+  }
+}
+
+//EventListeners
+toggleSwitch.addEventListener("change", switchTheme);
