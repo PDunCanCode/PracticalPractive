bdb60a2f470f5b0afb11a55421f5d0f03ba960d0
pDunCanCode <chesiredarling@gmail.com>
2020-09-01 21:02:45 -0700

    add play/pause JS functions

--- musikplayer/script.js
+++ musikplayer/script.js
@@ -1,1 +1,27 @@
 const image = document.querySelector("img");
+const prevBtn = document.getElementById("prev");
+const music = document.querySelector("audio");
+const playBtn = document.getElementById("play");
+const nextBtn = document.getElementById("next");
+
+let isPalying = false;
+
+function playSong() {
+  isPlaying = true;
+  playBtn.classList.replace("fa-play", "fa-pause");
+  playBtn.setAttribute("title", "Pause");
+  music.play();
+}
+
+function pauseSong() {
+  isPlaying = false;
+  playBtn.classList.replace("fa-pause", "fa-play");
+  playBtn.setAttribute("title", "Play");
+  music.pause();
+}
+
+// Play or Pause eventListener
+
+playBtn.addEventListerner("click", () =>
+  isPlaying ? pauseSong() : playSong()
+);
