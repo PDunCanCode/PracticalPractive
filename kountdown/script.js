01563501dac88d318db526c9b77009127f282c64
pDunCanCode <chesiredarling@gmail.com>
2020-09-02 13:10:02 -0700

    update updateCountdown to correct for NaN error

--- kountdown/script.js
+++ kountdown/script.js
@@ -10,6 +10,7 @@ const timeElements = document.querySelectorAll("span");
 let countdownTitle = "";
 let countdownDate = "";
 let countdownValue = Date;
+let countdownActive;
 
 const second = 1000;
 const minute = second * 60;
@@ -23,22 +24,24 @@ dateEl.setAttribute("min", today);
 
 //Populate the Countdown
 function updateDOM() {
-  const now = new Date().getTime();
-  const distance = countdownValue - now;
-  const days = Math.floor(distance / day);
-  const hours = Math.floor((distance % day) / hour);
-  const minutes = Math.floor((distance % hour) / minute);
-  const seconds = Math.floor((distance % minute) / second);
-  //populate Countdown
-  countdownElTitle.textContent = `${countdownTitle}`;
-  timeElements[0].textContent = `${days}`;
-  timeElements[1].textContent = `${hours}`;
-  timeElements[2].textContent = `${minutes}`;
-  timeElements[3].textContent = `${seconds}`;
+  countdownActive = setInterval(() => {
+    const now = new Date().getTime();
+    const distance = countdownValue - now;
+    const days = Math.floor(distance / day);
+    const hours = Math.floor((distance % day) / hour);
+    const minutes = Math.floor((distance % hour) / minute);
+    const seconds = Math.floor((distance % minute) / second);
+    //populate Countdown
+    countdownElTitle.textContent = `${countdownTitle}`;
+    timeElements[0].textContent = `${days}`;
+    timeElements[1].textContent = `${hours}`;
+    timeElements[2].textContent = `${minutes}`;
+    timeElements[3].textContent = `${seconds}`;
 
-  //Hide / Show Countdown
-  inputContainer.hidden = true;
-  countdownEl.hidden = false;
+    //Hide / Show Countdown
+    inputContainer.hidden = true;
+    countdownEl.hidden = false;
+  }, second);
 }
 
 function updateCountdown(e) {
@@ -46,10 +49,24 @@ function updateCountdown(e) {
   countdownTitle = e.srcElement[0].value;
   countdownDate = e.ercElement[1].value;
   //Get the number version of date, updateDom
-  countdownValue = new Date(countdownDate).getTime();
-  updateDOM();
+  if (countdownDate === "") {
+    alert("Please select a date for the countdown.");
+  } else {
+    countdownValue = new Date(countdownDate).getTime();
+    updateDOM();
+  }
 }
 
+//Reset all Values
+function reset() {
+  countdownEl.hidden = true;
+  inputContainer.hidden = false;
+  clearInterval(countdownActive);
+  //Reset Values
+  countdownTitle = "";
+  countdownDate = "";
+}
 //Event Listeners
 
 countdownForm.addEventListener("submit", updateCountdown);
+countdownBtn.addEventListener("click", reset);
