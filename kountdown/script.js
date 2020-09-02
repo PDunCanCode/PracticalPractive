f6aedb4e3bb3fe3b6707d112f9359186e312673b
pDunCanCode <chesiredarling@gmail.com>
2020-09-02 11:00:22 -0700

    add date/ dateEl to JS

--- kountdown/script.js
+++ kountdown/script.js
@@ -1,1 +1,8 @@
 const dateEl = document.getElementById("date-picker");
+const inputContainer = document.getElementById("input-container");
+const countdownForm = document.getElementById("countdown-form");
+
+//Set Date Min with todays date
+
+const today = new Date().toISOString().split("T")[0];
+dateEl.setAttribute("min", today);
