b5c0d92449d12290838dc825656df9e50e8bb2b3
pDunCanCode <chesiredarling@gmail.com>
2020-09-02 12:36:50 -0700

    add event listeners for countdown title && date

--- kountdown/script.js
+++ kountdown/script.js
@@ -2,7 +2,20 @@ const dateEl = document.getElementById("date-picker");
 const inputContainer = document.getElementById("input-container");
 const countdownForm = document.getElementById("countdown-form");
 
+let countdownTitle = "";
+let countdownDate = "";
+
 //Set Date Min with todays date
 
 const today = new Date().toISOString().split("T")[0];
 dateEl.setAttribute("min", today);
+
+function updateCountdown(e) {
+  e.preventDefault();
+  countdownTitle = e.srcElement[0].value;
+  countdownDate = e.ercElement[1].value;
+}
+
+//Event Listeners
+
+countdownForm.addEventListener("submit", updateCountdown);
