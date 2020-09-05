87fa1279991e380eba82056a13a79daeb114bb1b
pDunCanCode <chesiredarling@gmail.com>
2020-09-05 13:53:50 -0700

    initial formValidate

--- formValidation/script.js
+++ formValidation/script.js
@@ -1,1 +1,27 @@
 const form = document.getElementById("form");
+const password1El = document.getElementById("password1");
+const password2El = document.getElementById("password2");
+const messageContainer = document.querySelector(".message-container");
+const message = document.getElementById("message");
+
+let isValid = false;
+
+function validateForm() {
+  isValid = form.checkValidity();
+  if (!isValid) {
+    // Style main message for an error
+    message.textContent = "Please fill out all fields.";
+    message.style.color = "red";
+    messageContainer.style.borderColor = "red";
+    return;
+  }
+}
+
+function processFormData(e) {
+  e.preventDefault();
+  validateForm();
+}
+
+//EventListeners
+
+form.addEventListener("submit", processFormData);
