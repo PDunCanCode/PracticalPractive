7f263d6a5b475576e002eaf8640e50d819868cfd
pDunCanCode <chesiredarling@gmail.com>
2020-09-06 15:41:59 -0700

    the initial for RPSLS

---
+++ RPSLS/script.js
@@ -0,0 +1,6 @@
+import { startConfetti, stopConfetti, removeConfetti } from "./confetti.js";
+
+const playerScoreEl = document.getElementById("playerScore");
+const playerChoiceEl = document.getElementById("playerChoice");
+const computerScoreEl = document.getElementById("computerScore");
+const computerChoiceEl = document.getElementById("computerChoice");
