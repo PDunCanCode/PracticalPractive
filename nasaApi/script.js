16abae68477e986dd774d3ae8d7c1777bdbe64d8
pDunCanCode <chesiredarling@gmail.com>
2020-09-07 14:44:37 -0700

    add js fetch

--- nasaApi/script.js
+++ nasaApi/script.js
@@ -1,1 +1,16 @@
-const thing = document.getElementById("thing");
+const count = 10;
+const apiKey = "DEMO_KEY";
+const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&count=${count}`;
+let resultsArray = [];
+
+//Fetch 10 images from API
+async function getNasaPictures() {
+  try {
+    const response = await fetch(apiUrl);
+    resultsArray = await response.json();
+  } catch (error) {
+    //catch errorssss
+  }
+}
+
+getNasaPictures();
